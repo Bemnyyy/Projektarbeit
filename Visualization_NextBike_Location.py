8d7c84a0f7ddd11d95458697398c2629a488e111
Bemnyyy <benjamin.ristau@gmx.de>
2025-12-18 17:28:52 +0100

    tried to visualize the locations of nextbikes - not succesfull yet

--- Visualization_NextBike_Location.py
+++ Visualization_NextBike_Location.py
@@ -1,3 +1,18 @@
-import sqlite3
-import matplotlib.pyplot as plt
-import 
+import pandas as pd
+import geopandas as gpd
+from shapely import Point
+
+
+# pandas: read csv
+path = r'S:\Studium\3. Semester\VSMB 330 Digitalisierung und Mobilsoftware\Projektarbeit\NextBikeData as CSV\bike_locations.csv'
+df = pd.read_csv(path)
+#print(df.columns)
+
+# shapely: create geometry
+geometry_col = [Point(xy) for xy in zip(df['lng'], df['lat'])]
+#print(geometry_col[:4])
+
+# geopandas: host geospatial layer
+spatial_data = gpd.GeoDataFrame(df, geometry= geometry_col)
+spatial_data.set_crs(4326)
+print(spatial_data.plot())
