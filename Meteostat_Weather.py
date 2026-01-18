21e675a7a9909da66c03be52af165e30a677c6b9
Bemnyyy <benjamin.ristau@gmx.de>
2026-01-18 14:58:57 +0100

    small changes, in meteostat_weather added input prompts

--- Meteostat_Weather.py
+++ Meteostat_Weather.py
@@ -5,12 +5,15 @@ from datetime import datetime
 from config import API_KEY_METEOSTAT
 
 DB_PATH = "Weather_Data_3.db"
-CURRENT_DATE = str(datetime.now())[0:10] # Time format example: "2025-09-30"
+#start_date = "2026-01-01" # Time format example: "2025-09-30", this could also be resolved with an input prompt like start_date = input("choose a start Date: ")
+#end_date = "2026-01-15" # Time format example equially to start_date, could also be resolved as a input prompt
+start_date = input("Set a starting Date (Format: '2025-01-01'): ")
+end_date = input("Set a ending Date (Format: '2025-01-01'): ")
 
 # Get Data from API (Output: Dict, "meta" + "data")
 def load_weather_data():
     url = "https://meteostat.p.rapidapi.com/stations/hourly"
-    querystring = {"station":"10727","start": CURRENT_DATE,"end": CURRENT_DATE,"tz":"Europe/Berlin"}
+    querystring = {"station":"10727","start": start_date,"end": end_date,"tz":"Europe/Berlin"}
     headers = {
     	"x-rapidapi-key": API_KEY_METEOSTAT,
     	"x-rapidapi-host": "meteostat.p.rapidapi.com"
@@ -36,9 +39,10 @@ def clean_weather_data(json_data):
 
 # creating a SQL-DataFrame to easily view and display the data
 def create_SQL(df):
+    table_name = input("Type in a new Table-Name or the Table that the Data should append to:")
     engine = create_engine(f"sqlite:///{DB_PATH}")
-    df.to_sql("karlsruhe_weather_09_2025", con=engine, if_exists="fail", index=False)
-    print(f"{len(df)} Datensätze wurden in die Datenbank geschrieben am {datetime.now().strftime('%Y-%m-%d')} um {datetime.now().strftime('%H:%M:%S')} für folgendes Datum: {CURRENT_DATE}")
+    df.to_sql(f"{table_name}", con=engine, if_exists="append", index=False)
+    print(f"{len(df)} Datensätze wurden in die Datenbank geschrieben am {datetime.now().strftime('%Y-%m-%d')} um {datetime.now().strftime('%H:%M:%S')} für folgenden Zeitraum: \n{start_date} - {end_date} ")
     return df
 
 # Loading all the functions and defining df as a DataFrame to convert the list Data from previous to a SQL-DataFrame
